dc7cec01ea9db7c53a8f94ece554132d57b14775
jeison poveda <jeisonfpovedac@gmail.com>
2023-01-01 23:02:59 -0500

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <meta name="description" content="Tiendita - Venta de Muchas cosas" />
+        <link
+          rel="stylesheet"
+          href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
