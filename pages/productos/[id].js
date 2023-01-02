5c5053d6b1f7a7be5320c89b6dd1b983d6055e07
jeison poveda <jeisonfpovedac@gmail.com>
2023-01-02 03:40:19 -0500

    ADD vista de producto única dinámico

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,6 +1,35 @@
+import Image from 'next/image'
 
-export default function Producto() {
+export default function Producto({producto}) {
+
+  const { title, price, image, description, id } = producto;
+
+  console.log(producto)
+
   return (
-    <div>[key]</div>
-  )
+    <div>
+      <h2>{title} </h2>
+      <p>{description}</p>
+      <p>{price}</p>
+      <Image
+        src={image}
+        alt={`imagen producto ${title}`}
+        width={100}
+        height={100}
+      />
+    </div>
+  );
+}
+
+export async function getServerSideProps({params: {id}}) {
+  const respuesta = await fetch(`https://fakestoreapi.com/products/${id}`)
+  const producto = await respuesta.json();
+
+  console.log(producto)
+
+  return {
+    props: {
+      producto,
+    }, // will be passed to the page component as props
+  };
 }
