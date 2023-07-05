6d1fa943b1b87436962e82c3762dc5fb0889ef8f
jeison poveda <jeisonfpovedac@gmail.com>
2023-07-05 02:11:05 -0500

    Se agregó auth con google

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,37 +1,40 @@
-import React, { useState, useContext } from 'react';
-import { css } from '@emotion/react';
-import Layout from '../components/Layout';
-import { Formulario, Campo, InputSubmit } from '../components/ui/formulario';
+import React, { useState, useContext } from "react";
+import { css } from "@emotion/react";
+import Layout from "../components/Layout";
+import { Formulario, Campo, InputSubmit } from "../components/ui/formulario";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 //validacion del form
-import useValidacionInput from '../hooks/useValidacionInput';
-import validarCrearCuenta from '../validacion/validarCrearCuenta';
+import useValidacionInput from "../hooks/useValidacionInput";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
 
 const STATE_INICIAL = {
-  username: '',
-  email: '',
-  password: ''
-}
+  username: "",
+  email: "",
+  password: "",
+};
 
 const CrearCuenta = () => {
-
-  const {valores, errores, submitForm, handleSubmit, handleChange, handleBlur} = useValidacionInput(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+  const {
+    valores,
+    errores,
+    submitForm,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useValidacionInput(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
   const { username, email, password } = valores;
 
   function crearCuenta() {
-    console.log("Creando Cuenta...")
+    console.log("Creando Cuenta...");
   }
 
   return (
     <div>
       <Layout>
-        <Formulario
-          onSubmit={handleSubmit}
-        >
-          <h1>
-            Crea tu cuenta
-          </h1>
+        <Formulario onSubmit={handleSubmit}>
+          <h1>Crea tu cuenta</h1>
           <Campo>
             <label htmlFor="username">Username</label>
             <input
@@ -74,13 +77,22 @@ const CrearCuenta = () => {
             {errores.password && <p>{errores.password}</p>}
           </Campo>
 
-          <InputSubmit
-            type="submit"
-            value="Crear Cuenta"
-          />
+          <InputSubmit type="submit" value="Crear Cuenta" />
         </Formulario>
       </Layout>
+      <GoogleOAuthProvider clientId="266455807822-e34kito8ljg7p3kloascj6l3u8i8gfl3.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+          useOneTap
+        />
+        ;
+      </GoogleOAuthProvider>
     </div>
-  )
-}
+  );
+};
 export default CrearCuenta;
